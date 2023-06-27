a3583a3ba5c92367a043b3129ae77204703d5894
Rien van Duivenbode <r.v.duijvenbode@ziggo.nl>
2023-06-27 10:35:59 +0200

    ikpullgeben

--- Bo_GamePeriode1.4 B.9/Assets/Scripts/otherstuf/toHub.cs
+++ Bo_GamePeriode1.4 B.9/Assets/Scripts/otherstuf/toHub.cs
@@ -8,7 +8,8 @@ public class toHub : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
