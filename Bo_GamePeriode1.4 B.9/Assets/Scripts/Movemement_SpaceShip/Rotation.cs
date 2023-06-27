2e5cf19d6681a1ed26f4602ab6a2f18a0e634a8f
Rien van Duivenbode <r.v.duijvenbode@ziggo.nl>
2023-06-27 10:00:19 +0200

    introscherm

--- Bo_GamePeriode1.4 B.9/Assets/Scripts/Movemement_SpaceShip/Rotation.cs
+++ Bo_GamePeriode1.4 B.9/Assets/Scripts/Movemement_SpaceShip/Rotation.cs
@@ -11,6 +11,7 @@ public class Rotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
